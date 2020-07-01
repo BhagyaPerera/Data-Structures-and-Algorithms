d8acc8fbfdf9465f4b0bfedd0164a758f1938318
BhagyaPerera <59433789+BhagyaPerera@users.noreply.github.com>
2020-07-01 13:58:55 +0530

    Add files via upload

---
+++ Recursion1/src/recursion1/Recursion1.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursion1;
+
+/**
+ *
+ * @author BHAGYA
+ */
+public class Recursion1 {
+    
+    
+    
+    
+    public Recursion1(){
+        
+ 
+        
+    }
+    
+    
+     public int factorial(int n){
+        
+        if(n==0)
+            return 1;
+        
+        else
+          return  n*factorial(n-1);
+    
+    
+     }
+    
+    
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Recursion1/src/recursion1/TestRecursion1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursion1;
+
+/**
+ *
+ * @author BHAGYA
+ */
+public class TestRecursion1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Recursion1 r=new Recursion1();
+        int i=r.factorial(5);
+        System.err.println(i);
+                
+                
+    }
+    
+}
